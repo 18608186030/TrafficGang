26c00a957a5ace0bc53718bd179a20aafdabeefe
superman <156663985@qq.com>
2020-02-05 21:59:11 +0800

    第一次提交

--- app/src/main/java/com/stjy/trafficgang/MainActivity.kt
+++ app/src/main/java/com/stjy/trafficgang/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.stjy.trafficgang
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
+import com.stjy.baselib.wigiet.bottomview.BotBean
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val fragments = ArrayList<Fragment>()
+    private val itemIcon = ArrayList<BotBean>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragments.add( Fragment())
+        fragments.add(Fragment())
+        fragments.add(Fragment())
+        itemIcon.add(BotBean("工作 ", R.mipmap.ic_home_normal, R.mipmap.ic_home_pressed))
+        itemIcon.add(BotBean("通讯录", R.mipmap.ic_home_normal, R.mipmap.ic_home_pressed))
+        itemIcon.add(BotBean("个人中心", R.mipmap.ic_home_normal, R.mipmap.ic_home_pressed))
+        vp_content.adapter = FragmentAdapter(supportFragmentManager)
+        vp_content.offscreenPageLimit = fragments.size
+        bottom.setViewPager(vp_content, itemIcon) {
+
+        }
+    }
+
+    internal inner class FragmentAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(i: Int): Fragment {
+            return fragments[i]
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
     }
 }
---
+++ baselib/src/main/java/com/stjy/baselib/wigiet/NoScrollViewPager.kt
@@ -0,0 +1,32 @@
+package com.stjy.baselib.wigiet
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+
+/**
+ * 自定义一个不能左右滑动的ViewPager
+ * @author Beyond
+ */
+class NoScrollViewPager : ViewPager {
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+
+    constructor(context: Context) : super(context) {}
+
+    override fun onTouchEvent(arg0: MotionEvent): Boolean {
+        return false
+    }
+
+    /**
+     * 不拦截事件
+     */
+    override fun onInterceptTouchEvent(arg0: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        return super.dispatchTouchEvent(ev)
+    }
+}
